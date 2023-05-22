b5285d1630f7fbeaf51812359ae9b46d624e5b62
Ebrahim Mansour A Sharka <ebraandomar@gmail.com>
2023-05-22 09:50:43 +0300

    Update schedule.blade.php

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -237,17 +237,15 @@
         <div class="h-10"> @include('includes.footer', [
             'orange' => '<p>
             <a href="http://jigsaw.w3.org/css-validator/check/referer">
-                <img style="border:0;width:88px;height:31px"
-                    src="http://jigsaw.w3.org/css-validator/images/vcss"
-                    alt="Valid CSS!" />
-            </a>
+    <img style="border:0;width:88px;height:31px"
+        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+        alt="Valid CSS!" />
+    </a>
         </p>',
             'blue' => '<p>
-        <a href="http://jigsaw.w3.org/css-validator/check/referer">
-            <img style="border:0;width:88px;height:31px"
-                src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
-                alt="Valid CSS!" />
-            </a>
+            <a href="http://validator.w3.org/check?uri=referer"><img
+        src="http://www.w3.org/Icons/valid-xhtml10"
+        alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
         </p>',
         ])</div>
     </div>
